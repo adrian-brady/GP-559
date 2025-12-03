5cda9278b7e79c53e15c1c9101d9b9a3bcc3b319
Adrian Brady <mail@adrianbrady.com>
2025-12-02 19:59:58 -0600

    add objects to scene

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -7,13 +7,19 @@ import {
   PlaneGeometry,
   PerspectiveCamera,
   DoubleSide,
+  CylinderGeometry,
+  SphereGeometry,
+  BufferGeometry,
+  Material,
+  Vector3,
 } from 'three';
 import { GameScene } from './GameScene.js';
 import { MeshInstance } from '../components/MeshInstance.js';
 import { PlayerController } from '../components/PlayerController.js';
 import { CameraFollow } from '../components/CameraFollow.js';
-import RAPIER from '@dimforge/rapier3d-compat';
+import RAPIER, { ColliderDesc } from '@dimforge/rapier3d-compat';
 import { RigidBody } from '../components/RigidBody.js';
+import { Entity } from '../ecs/Entity.js';
 
 class MainScene extends GameScene {
   /** @type {PerspectiveCamera} */
@@ -34,7 +40,34 @@ class MainScene extends GameScene {
     this.setupLighting();
     this.setupPlayer();
     this.setupEnvironment();
+    this.setupObjects();
     this.scene.background = new Color('skyblue');
+  }
+
+  setupObjects() {
+    this.createPhysicsObject(
+      'cube',
+      new BoxGeometry(2, 2, 2),
+      new MeshStandardMaterial({ color: 0xff6347 }),
+      { x: 5, y: 1, z: -3 },
+      RAPIER.ColliderDesc.cuboid(1, 1, 1)
+    );
+
+    this.createPhysicsObject(
+      'cylinder',
+      new CylinderGeometry(1, 1, 3, 16),
+      new MeshStandardMaterial({ color: 0x4169e1 }),
+      { x: -8, y: 1.5, z: -10 },
+      RAPIER.ColliderDesc.cylinder(1.5, 1)
+    );
+
+    this.createPhysicsObject(
+      'sphere',
+      new SphereGeometry(1.5, 32, 32),
+      new MeshStandardMaterial({ color: 0x32cd32 }),
+      { x: -3, y: 1.5, z: 2 },
+      RAPIER.ColliderDesc.ball(1.5)
+    );
   }
 
   setupLighting() {
@@ -93,6 +126,31 @@ class MainScene extends GameScene {
     const collider = this.physicsWorld.createCollider(colliderDesc, rigidBody);
 
     // ground.addComponent(RigidBody, rigidBody, collider);
+  }
+
+  /**
+   *
+   * @param {string} name - Entity name
+   * @param {BufferGeometry} geometry - Visual Geometry
+   * @param {Material} material - Material
+   * @param {Vector3} position - Position {x, y, z}
+   * @param {ColliderDesc} colliderDesc - Physics Collider
+   * @returns {Entity}
+   */
+  createPhysicsObject(name, geometry, material, position, colliderDesc) {
+    const entity = this.entityManager.createEntity(this.scene, name);
+    entity.addComponent(MeshInstance, geometry, material);
+    entity.transform.position.set(position.x, position.y, position.z);
+
+    const rigidBodyDesc = RAPIER.RigidBodyDesc.fixed().setTranslation(
+      position.x,
+      position.y,
+      position.z
+    );
+    const rigidBody = this.physicsWorld.createRigidBody(rigidBodyDesc);
+    this.physicsWorld.createCollider(colliderDesc, rigidBody);
+
+    return entity;
   }
 }
 
